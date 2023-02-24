1d44048116185b5c600ef44b6125a70dee18148d
Vadim Roschupkin <vroshupkin1@gmail.com>
2023-02-25 00:34:29 +0300

    Очистка папки templates

---
+++ src/common/terminal.ts
@@ -0,0 +1,25 @@
+import { exec } from 'node:child_process';
+
+// any callback
+export function clearDirTemplate(callback: any): boolean {
+	exec('ls ../templates/*.template', (_, stdout) => {
+		const dir = stdout.split('\n');
+		if (dir.length) {
+			exec('rm ../templates/*.template', () => {
+				let sentence = '';
+				if (dir.length === 1) {
+					sentence = `Удален ${dir.length} шаблон`;
+				} else if (dir.length < 5) {
+					sentence = `Удалены ${dir.length} шаблона`;
+				} else {
+					sentence = `Удалены ${dir.length} шаблонов`;
+				}
+
+				console.log(sentence);
+				callback();
+			});
+		}
+	});
+
+	return true;
+}
