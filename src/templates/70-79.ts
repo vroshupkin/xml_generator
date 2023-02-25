87dff3a274e4072e39bc0bfd98838f1697d3eec5
Vadim Roschupkin <vroshupkin1@gmail.com>
2023-02-26 01:45:36 +0300

    80-88

--- src/templates/70-79.ts
+++ src/templates/70-79.ts
@@ -1,5 +1,3 @@
-// 24.02
-
 import { generate, IGenerate } from '../../generator';
 import { Arc, Circle, LinePath } from '../figures';
 import { clearDirTemplate } from '../common/terminal';
@@ -139,9 +137,272 @@ const figure_72: IGenerate = {
 	</params>
 	`,
 };
-clearDirTemplate(() => {
-	generate(figure_70);
-	generate(figure_71);
-	generate(figure_72);
-	generate(figure_73);
-});
+
+const figure_74: IGenerate = {
+	name: `Прямоугольник с проёмом, окружностью и креплениями`,
+	primitives: [
+		new LinePath([
+			['W/2 + dA / 2', 'H'],
+			['W', 'H'],
+			['W', '0'],
+			['0', '0'],
+			['0', 'H'],
+			['W/2 - dA/2', 'H'],
+		]),
+
+		new LinePath([
+			['W/2 - dA/2', 'H'],
+			['W/2 - dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H'],
+		]),
+
+		new Circle(['0 + dL', '0 + dL'], 'R'),
+		new Circle(['0 + dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', '0 + dL'], 'R'),
+
+		new Circle(['W/2 + X', 'H/2 + Y'], 'Rc'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dA" desc="[мм] Ширина проема" default="80.0"></p>
+        <p name="dH" desc="[мм] Глубина проема" default="40.0"></p>
+
+        <p name="R" desc="[мм] Радиус креплений" default="15.0"></p>
+        <p name="dL" desc="[мм] Отступ креплений" default="40.0"></p>
+
+        <p name="Rc" desc="[мм] Радиус центральной окружности" default="50.0"></p>
+        <p name="X" desc="[мм] Смещение окружности Х" default="0.0"></p>
+        <p name="Y" desc="[мм] Смещение окружности Y" default="0.0"></p>
+
+	</params>
+	`,
+};
+
+const figure_75: IGenerate = {
+	name: `Прямоугольник с проёмом и окружностью`,
+	primitives: [
+		new LinePath([
+			['W/2 + dA / 2', 'H'],
+			['W', 'H'],
+			['W', '0'],
+			['0', '0'],
+			['0', 'H'],
+			['W/2 - dA/2', 'H'],
+		]),
+
+		new LinePath([
+			['W/2 - dA/2', 'H'],
+			['W/2 - dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H'],
+		]),
+
+		new Circle(['W/2 + X', 'H/2 + Y'], 'Rc'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dA" desc="[мм] Ширина проема" default="80.0"></p>
+        <p name="dH" desc="[мм] Глубина проема" default="40.0"></p>
+
+        <p name="Rc" desc="[мм] Радиус центральной окружности" default="50.0"></p>
+        <p name="X" desc="[мм] Смещение окружности Х" default="0.0"></p>
+        <p name="Y" desc="[мм] Смещение окружности Y" default="0.0"></p>
+
+	</params>
+	`,
+};
+
+const figure_76: IGenerate = {
+	name: `Прямоугольник с проёмом и креплениями`,
+	primitives: [
+		new LinePath([
+			['W/2 + dA / 2', 'H'],
+			['W', 'H'],
+			['W', '0'],
+			['0', '0'],
+			['0', 'H'],
+			['W/2 - dA/2', 'H'],
+		]),
+
+		new LinePath([
+			['W/2 - dA/2', 'H'],
+			['W/2 - dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H'],
+		]),
+
+		new Circle(['0 + dL', '0 + dL'], 'R'),
+		new Circle(['0 + dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', '0 + dL'], 'R'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dA" desc="[мм] Ширина проема" default="80.0"></p>
+        <p name="dH" desc="[мм] Глубина проема" default="40.0"></p>
+
+        <p name="R" desc="[мм] Радиус креплений" default="15.0"></p>
+        <p name="dL" desc="[мм] Отступ креплений" default="40.0"></p>
+
+	</params>
+	`,
+};
+
+const figure_77: IGenerate = {
+	name: `Прямоугольник с трапциевидными проемами окружностью и креплениями`,
+	primitives: [
+		new LinePath([
+			[0, 0],
+			[0, 'H'],
+			['W/2 - (dA/2 + dW)', 'H'],
+			['W/2 - dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H - dH'],
+			['W/2 + dA/2 + dW', 'H'],
+			['W', 'H'],
+			['W', '0'],
+
+			['W/2 + dA/2 + dW', '0'],
+			['W/2 + dA/2', '0 + dH'],
+			['W/2 - dA/2', '0 + dH'],
+			['W/2 - (dA/2 + dW)', '0'],
+			[0, 0],
+		]),
+
+		new Circle(['0 + dL', '0 + dL'], 'R'),
+		new Circle(['0 + dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', '0 + dL'], 'R'),
+
+		new Circle(['W/2 + X', 'H/2 + Y'], 'Rc'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dA" desc="[мм] Ширина проема" default="40.0"></p>
+        <p name="dH" desc="[мм] Глубина проема" default="40.0"></p>
+        <p name="dW" desc="[мм] Отступ трапеции" default="40.0"></p>
+
+        <p name="R" desc="[мм] Радиус креплений" default="15.0"></p>
+        <p name="dL" desc="[мм] Отступ креплений" default="40.0"></p>
+
+        <p name="Rc" desc="[мм] Радиус центральной окружности" default="50.0"></p>
+        <p name="X" desc="[мм] Смещение окружности Х" default="0.0"></p>
+        <p name="Y" desc="[мм] Смещение окружности Y" default="0.0"></p>
+
+	</params>
+	`,
+};
+
+const figure_78: IGenerate = {
+	name: `Прямоугольник с трапциевидными проемами и окружностью`,
+	primitives: [
+		new LinePath([
+			[0, 0],
+			[0, 'H'],
+			['W/2 - (dA/2 + dW)', 'H'],
+			['W/2 - dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H - dH'],
+			['W/2 + dA/2 + dW', 'H'],
+			['W', 'H'],
+			['W', '0'],
+
+			['W/2 + dA/2 + dW', '0'],
+			['W/2 + dA/2', '0 + dH'],
+			['W/2 - dA/2', '0 + dH'],
+			['W/2 - (dA/2 + dW)', '0'],
+			[0, 0],
+		]),
+
+		new Circle(['W/2 + X', 'H/2 + Y'], 'Rc'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dA" desc="[мм] Ширина проема" default="40.0"></p>
+        <p name="dH" desc="[мм] Глубина проема" default="40.0"></p>
+        <p name="dW" desc="[мм] Отступ трапеции" default="40.0"></p>
+
+
+        <p name="Rc" desc="[мм] Радиус центральной окружности" default="50.0"></p>
+        <p name="X" desc="[мм] Смещение окружности Х" default="0.0"></p>
+        <p name="Y" desc="[мм] Смещение окружности Y" default="0.0"></p>
+
+	</params>
+	`,
+};
+
+const figure_79: IGenerate = {
+	name: `Прямоугольник с трапциевидными проемами креплениями`,
+	primitives: [
+		new LinePath([
+			[0, 0],
+			[0, 'H'],
+			['W/2 - (dA/2 + dW)', 'H'],
+			['W/2 - dA/2', 'H - dH'],
+			['W/2 + dA/2', 'H - dH'],
+			['W/2 + dA/2 + dW', 'H'],
+			['W', 'H'],
+			['W', '0'],
+
+			['W/2 + dA/2 + dW', '0'],
+			['W/2 + dA/2', '0 + dH'],
+			['W/2 - dA/2', '0 + dH'],
+			['W/2 - (dA/2 + dW)', '0'],
+			[0, 0],
+		]),
+
+		new Circle(['0 + dL', '0 + dL'], 'R'),
+		new Circle(['0 + dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', '0 + dL'], 'R'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dA" desc="[мм] Ширина проема" default="40.0"></p>
+        <p name="dH" desc="[мм] Глубина проема" default="40.0"></p>
+        <p name="dW" desc="[мм] Отступ трапеции" default="40.0"></p>
+
+        <p name="R" desc="[мм] Радиус креплений" default="15.0"></p>
+        <p name="dL" desc="[мм] Отступ креплений" default="40.0"></p>
+
+	</params>
+	`,
+};
+
+// clearDirTemplate(() => {
+// 	generate(figure_70);
+// 	generate(figure_71);
+// 	generate(figure_72);
+// 	generate(figure_73);
+
+// 	generate(figure_74);
+// 	generate(figure_75);
+// 	generate(figure_76);
+// 	generate(figure_77);
+// 	generate(figure_78);
+// 	generate(figure_79);
+// });
---
+++ src/templates/80-89.ts
@@ -0,0 +1,322 @@
+import { generate, IGenerate } from '../../generator';
+import { Arc, Circle, LinePath } from '../figures';
+import { clearDirTemplate } from '../common/terminal';
+import { Condition } from '../condition';
+
+const figure_80: IGenerate = {
+	name: `Угловое соединение с окружностью `,
+	primitives: [
+		new LinePath([
+			[0, 'R1'],
+			[0, 'H'],
+			['W - dW', 'H'],
+			['W', 'H - dH'],
+			['W', 0],
+			['R1', 0],
+		]),
+		new Arc([0, 0], 'R1', '0', 'M_PI / 2', false),
+
+		new Circle(['W/2 + X', 'H/2 + Y'], 'R'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dW" desc="[мм] Ширина скоса" default="100.0"></p>
+		<p name="dH" desc="[мм] Высота скоса" default="100.0"></p>
+    
+        <p name="R" desc="[мм] Радиус центральной окружности" default="50.0"></p>
+        <p name="X" desc="[мм] Смещение окружности Х" default="50.0"></p>
+        <p name="Y" desc="[мм] Смещение окружности Y" default="0.0"></p>
+
+		<p name="R1" desc="[мм] Радиус угловой окружности" default="150.0"></p>
+	</params>
+	`,
+};
+
+const figure_81: IGenerate = {
+	name: `Угловое соединение`,
+	primitives: [
+		new LinePath([
+			[0, 'R1'],
+			[0, 'H'],
+			['W - dW', 'H'],
+			['W', 'H - dH'],
+			['W', 0],
+			['R1', 0],
+		]),
+		new Arc([0, 0], 'R1', '0', 'M_PI / 2', false),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dW" desc="[мм] Ширина скоса" default="100.0"></p>
+		<p name="dH" desc="[мм] Высота скоса" default="100.0"></p>
+    
+		<p name="R1" desc="[мм] Радиус угловой окружности" default="150.0"></p>
+	</params>
+	`,
+};
+
+const figure_82: IGenerate = {
+	name: `Угловое соединение c проемами`,
+	primitives: [
+		new LinePath([
+			[0, 'R1'],
+			[0, 'H'],
+			['W - dW', 'H'],
+			['W', 'H - dH'],
+			['W', 0],
+			['R1', 0],
+		]),
+		new Arc([0, 0], 'R1', '0', 'M_PI / 2', false),
+
+		new Circle(['dL', 'H - dL'], 'R'),
+		new Circle(['W - dL', 'dL'], 'R'),
+		new Circle(['W - dW/(1.5)', 'H - dH/(1.5)'], 'R'),
+	],
+	params: `   
+	<params>
+		
+		<p name="W" desc="[мм] Ширина" default="300.0"></p>
+		<p name="H" desc="[мм] Высота" default="200.0"></p>
+
+        <p name="dW" desc="[мм] Ширина скоса" default="100.0"></p>
+		<p name="dH" desc="[мм] Высота скоса" default="100.0"></p>
+    
+		<p name="R1" desc="[мм] Радиус угловой окружности" default="150.0"></p>
+
+        <p name="R" desc="[мм] Радиус креплений" default="15.0"></p>
+        <p name="dL" desc="[мм] Отступ креплений" default="25.0"></p>
+
+	</params>
+	`,
+};
+
+const figure_83: IGenerate = {
+	name: `Крест`,
+	primitives: [
+		new LinePath([
+			['-(dW/2)', '-(dW/2)'],
+			['-(dW/2 + dH)', '-(dW/2)'],
+			['-(dW/2 + dH)', '(dW/2)'],
+
+			['-(dW/2)', '(dW/2)'],
+			['-(dW/2)', '(dW/2) + dH'],
+			['(dW/2)', '(dW/2) + dH'],
+
+			['(dW/2)', '(dW/2)'],
+			['(dW/2) + dH', '(dW/2)'],
+			['(dW/2) + dH', '-(dW/2)'],
+
+			['(dW/2)', '-(dW/2)'],
+			['(dW/2)', '-(dW/2 + dH)'],
+			['-(dW/2)', '-(dW/2 + dH)'],
+
+			['-(dW/2)', '-(dW/2)'],
+		]),
+	],
+	params: `   
+	<params>
+		
+
+        <p name="dW" desc="[мм] Ширина выступа креста" default="40.0"></p>
+		<p name="dH" desc="[мм] Высота выступа креста" default="100.0"></p>
+    
+
+	</params>
+	`,
+};
+
+const figure_84: IGenerate = {
+	name: `Крест с окружностью`,
+	primitives: [
+		new LinePath([
+			['-(dW/2)', '-(dW/2)'],
+			['-(dW/2 + dH)', '-(dW/2)'],
+			['-(dW/2 + dH)', '(dW/2)'],
+
+			['-(dW/2)', '(dW/2)'],
+			['-(dW/2)', '(dW/2) + dH'],
+			['(dW/2)', '(dW/2) + dH'],
+
+			['(dW/2)', '(dW/2)'],
+			['(dW/2) + dH', '(dW/2)'],
+			['(dW/2) + dH', '-(dW/2)'],
+
+			['(dW/2)', '-(dW/2)'],
+			['(dW/2)', '-(dW/2 + dH)'],
+			['-(dW/2)', '-(dW/2 + dH)'],
+
+			['-(dW/2)', '-(dW/2)'],
+		]),
+
+		new Circle([0, 0], 'R'),
+	],
+	params: `   
+	<params>
+		
+
+        <p name="dW" desc="[мм] Ширина выступа креста" default="40.0"></p>
+		<p name="dH" desc="[мм] Высота выступа креста" default="100.0"></p>
+    
+    	<p name="R" desc="[мм] Радиус окружности" default="20.0"></p>
+    
+	</params>
+	`,
+};
+
+const figure_85: IGenerate = {
+	name: `Крест с креплениями`,
+	primitives: [
+		new LinePath([
+			['-(dW/2)', '-(dW/2)'],
+			['-(dW/2 + dH)', '-(dW/2)'],
+			['-(dW/2 + dH)', '(dW/2)'],
+
+			['-(dW/2)', '(dW/2)'],
+			['-(dW/2)', '(dW/2) + dH'],
+			['(dW/2)', '(dW/2) + dH'],
+
+			['(dW/2)', '(dW/2)'],
+			['(dW/2) + dH', '(dW/2)'],
+			['(dW/2) + dH', '-(dW/2)'],
+
+			['(dW/2)', '-(dW/2)'],
+			['(dW/2)', '-(dW/2 + dH)'],
+			['-(dW/2)', '-(dW/2 + dH)'],
+
+			['-(dW/2)', '-(dW/2)'],
+		]),
+
+		new Circle(['-(dH - dL) ', 0], 'R'),
+		new Circle([0, '(dH - dL) '], 'R'),
+		new Circle(['(dH - dL) ', 0], 'R'),
+		new Circle([0, '-(dH - dL) '], 'R'),
+	],
+	params: `   
+	<params>
+		
+
+        <p name="dW" desc="[мм] Ширина выступа креста" default="40.0"></p>
+		<p name="dH" desc="[мм] Высота выступа креста" default="100.0"></p>
+    
+    	<p name="R" desc="[мм] Радиус окружности" default="14.0"></p>
+        <p name="dL" desc="[мм] Отступ окружности" default="0.0"></p>
+    
+	</params>
+	`,
+};
+
+const figure_86: IGenerate = {
+	name: `Треугольник с выступами`,
+	primitives: [
+		new LinePath([
+			[0, 0],
+			['W', 0],
+			['W', 'H'],
+			['W - dW', 'H'],
+			['W - dW', 'H - dH'],
+			['dW', 'dH'],
+			[0, 'dH'],
+			[0, 0],
+		]),
+	],
+	params: `   
+	<params>
+        <p name="W" desc="[мм] Ширина " default="200.0"></p>
+        <p name="H" desc="[мм] Высота " default="200.0"></p>
+
+
+        <p name="dW" desc="[мм] Ширина выступа" default="40.0"></p>
+		<p name="dH" desc="[мм] Высота выступа" default="40.0"></p>
+    
+    	
+	</params>
+	`,
+};
+
+const figure_87: IGenerate = {
+	name: `Треугольник с выступами и окружностью`,
+	primitives: [
+		new LinePath([
+			[0, 0],
+			['W', 0],
+			['W', 'H'],
+			['W - dW', 'H'],
+			['W - dW', 'H - dH'],
+			['dW', 'dH'],
+			[0, 'dH'],
+			[0, 0],
+		]),
+
+		new Circle(['W/(1.5) + X', 'H/3 + Y'], 'R'),
+	],
+	params: `   
+	<params>
+        <p name="W" desc="[мм] Ширина " default="200.0"></p>
+        <p name="H" desc="[мм] Высота " default="200.0"></p>
+
+        <p name="R" desc="[мм] Радиус окружности" default="40.0"></p>
+        <p name="X" desc="[мм] Сдвиг окружности X" default="10.0"></p>
+        <p name="Y" desc="[мм] Сдвиг окружности Y" default="-10.0"></p>
+
+        <p name="dW" desc="[мм] Ширина выступа" default="40.0"></p>
+		<p name="dH" desc="[мм] Высота выступа" default="40.0"></p>
+    
+    	
+	</params>
+	`,
+};
+
+const figure_88: IGenerate = {
+	name: `Треугольник с выступами окружностью и креплениями`,
+	primitives: [
+		new LinePath([
+			[0, 0],
+			['W', 0],
+			['W', 'H'],
+			['W - dW', 'H'],
+			['W - dW', 'H - dH'],
+			['dW', 'dH'],
+			[0, 'dH'],
+			[0, 0],
+		]),
+
+		new Circle(['W/(1.5) + X', 'H/3 + Y'], 'R'),
+
+		new Circle(['dW / 2', 'dH / 2'], 'R1'),
+		new Circle(['W - dW / 2', 'H - dH / 2'], 'R1'),
+	],
+	params: `   
+	<params>
+        <p name="W" desc="[мм] Ширина " default="200.0"></p>
+        <p name="H" desc="[мм] Высота " default="200.0"></p>
+
+        <p name="R" desc="[мм] Радиус окружности" default="40.0"></p>
+        <p name="X" desc="[мм] Сдвиг окружности X" default="10.0"></p>
+        <p name="Y" desc="[мм] Сдвиг окружности Y" default="-10.0"></p>
+
+        <p name="dW" desc="[мм] Ширина выступа" default="40.0"></p>
+		<p name="dH" desc="[мм] Высота выступа" default="40.0"></p>
+    
+    	<p name="R1" desc="[мм] Радиус креплений" default="12.0"></p>
+	</params>
+	`,
+};
+clearDirTemplate(() => {
+	generate(figure_80);
+	generate(figure_81);
+	generate(figure_82);
+	generate(figure_83);
+	generate(figure_84);
+	generate(figure_85);
+	generate(figure_86);
+	generate(figure_87);
+	generate(figure_88);
+});
